7115bf3d72d088bf6b46b54bc24f903f2c9c8bc6
geek.zhao <jczhao_cs@163.com>
2024-05-27 10:28:44 +0800

    【算法】：反转字符串2

--- src/main/java/com/geek/algorithm/string/ReverseString1.java
+++ src/main/java/com/geek/algorithm/string/ReverseString1.java
@@ -7,7 +7,7 @@ package com.geek.algorithm.string;
  * @Desc 反转字符串：双指针
  * https://programmercarl.com/0344.%E5%8F%8D%E8%BD%AC%E5%AD%97%E7%AC%A6%E4%B8%B2.html#%E6%80%9D%E8%B7%AF
  */
-public class ReverseString {
+public class ReverseString1 {
     public void reverseString(char[] s) {
         for (int i = 0, j = s.length -1; i < j; i++, j--) {
             char tmp = s[i];
--- src/main/java/com/geek/algorithm/string/ReverseString2.java
+++ src/main/java/com/geek/algorithm/string/ReverseString2.java
@@ -7,7 +7,7 @@ package com.geek.algorithm.string;
  * @Desc 反转字符串
  * https://programmercarl.com/0541.%E5%8F%8D%E8%BD%AC%E5%AD%97%E7%AC%A6%E4%B8%B2II.html#%E5%85%B6%E4%BB%96%E8%AF%AD%E8%A8%80%E7%89%88%E6%9C%AC
  */
-public class ReverseStr {
+public class ReverseString2 {
     public String reverseStr(String s, int k) {
         char[] nums = s.toCharArray();
         // 每次循环i的步长为2k
